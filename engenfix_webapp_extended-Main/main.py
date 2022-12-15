13318dabd8e7d1b38d9887019de13179cb44816f
Najmul Haq Southun Najjas <96812557+najmulhaq-sn@users.noreply.github.com>
2022-12-15 08:57:21 +0530

    Update main.py
    port change

--- engenfix_webapp_extended-Main/main.py
+++ engenfix_webapp_extended-Main/main.py
@@ -148,7 +148,7 @@ async def get_damage_detection(request:Request, file : UploadFile = File(...)):
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=8000)
 
 # uvicorn main:app --reload
 
